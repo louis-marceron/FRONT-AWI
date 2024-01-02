c9483f5ed9d95f31858b0fa05e817b6cfd765be2
Yunaie <119615317+Yunaie@users.noreply.github.com>
2024-01-02 16:16:26 +0100

    feat : mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-import LoginPage from './components/LoginPage'
-import WelcomePage from './components/WelcomePage'
-import RegisterPage from './components/RegisterPage'
-import PlanningPage from './components/PlanningPage'
-import ZonePage from './components/ZonePage'
-import Layout from './components/Layout'
-import UserPerEvenement from './components/UserPerEvenement'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+import { Router } from './Router';
+import { theme } from './theme';
 
-
-function App() {
-
+export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path = "/" element = {<Layout/>}>
-            <Route path = "/" element = {<WelcomePage/>} />  
-            <Route path = "/login" element = {<LoginPage/>} />  
-            <Route path = "/register" element = {<RegisterPage/>} /> 
-            <Route path = "/zones" element = {<ZonePage/>} /> 
-            <Route path = "/planning" element = {<PlanningPage/>} /> 
-            <Route path = "/UserPerEvenement" element = {<UserPerEvenement/>} /> 
-
-        </Route>
-        
-      </Routes>
-   </Router>
-  )
-
-
+    <MantineProvider theme={theme}>
+      <Router />
+    </MantineProvider>
+  );
 }
-
-export default App
